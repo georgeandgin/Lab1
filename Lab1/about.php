4862954410045303ef905494bfc935b04d6a9c3a
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-01-25 00:11:03 +0200

    asking this to work

--- Lab1/about.php
+++ Lab1/about.php
@@ -27,18 +27,15 @@
 <p class="biggerText" id="ginBio">One of our most valuable members in the book club. She has read more than 3 books. Her dedication is outstanding!</p>
 <p id="descriptionGin">Gintare has been in our book club since 2019. You will find her in every design book discussion. In fact, she has participated in more 
     discussions than there are days in a month. Among her favourite book genres are dystopian fiction, satire, philosophy and novels.</p>
-</div>
 
+<h1 id="nameLeft">Georgi Kolev</h1>
 <img id="geo" class="profileimg" src="img/bookClubGeorge.jpg" alt="Portrait Photo"/>
-<div class="right">
-<h1>Georgi Kolev</h1>
 <p class="biggerText" id="georgeBio">Our expert in self development books. Feel free to ask him about any of those, 
   he will surely have an opinion on your book!</p>
 <p id="descriptionGeorge">George is the president of our book club. He joined as soon as he started studying in 2018. 
   He is one of the most passionate book person that you will ever meet. You can find loads of different 
   things in his reading list, but you will surely see science fiction, self improvement and romance at 
   the top of the list!</p>
-</div>
 
 
 <?php include ('footer.php');?>
