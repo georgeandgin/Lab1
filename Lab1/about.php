ff152cf34a8f14405478afcf8f15ba0ec9502bac
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-01-24 18:04:22 +0200

    Added bios

--- Lab1/about.php
+++ Lab1/about.php
@@ -14,27 +14,32 @@
 
 <body>
 
-<section><h1>About us</h1></section>
+<?php include ('header.php');?>
 
-<?php
-//TESTING TESTING GITHUB TESTING
-
- include ('header.php');?>
-<?php include ('mainnav.php');?>
+<p class="biggerText">JU's book club is now 10 years old</p>
+<p>Weekly book readings have been continuing for a decade. Each month we choose books that we are going to read.
+    We then have informal group discussions to share our ideas and thinking. 
+</p>
+<img class="profileimg" src="img/bookClub.jpeg" alt="Portrait Photo"/>
 
 <div class="members">
 
 <div class="left">
 <h1>Gintare Kunigonyte</h1>
 <p class="biggerText">One of our most valuable members in the book club. She has read more than 3 books. Her dedication is outstanding!</p>
-<p>Sed egestas fringilla tristique. Nullam neque nisi, pulvinar tincidunt volutpat sit amet, hendrerit vitae quam. Nam varius in turpis vitae laoreet. Suspendisse mauris diam, pellentesque vitae quam vel, fringilla gravida nulla. Curabitur at commodo enim. Vivamus leo tellus, mattis vel aliquam non, porta in nibh. Aenean tortor ante, rutrum eu lacus ac, auctor condimentum est.</p>
+<p>Gintare has been in our book club since 2019. You will find her in every design book discussion. In fact, she has participated in more 
+    discussions than there are days in a month. Among her favourite book genres are dystopian fiction, satire, philosophy and novels.</p>
 <img id="gin" class="profileimg" src="img/bookClubGintare.jpeg" alt="Portrait Photo"/>
 </div>
 
 <div class="right">
 <h1>Georgi Kolev</h1>
-<p class="biggerText">One of our most valuable members in the book club. She has read more than 3 books. Her dedication is outstanding!</p>
-<p>Sed egestas fringilla tristique. Nullam neque nisi, pulvinar tincidunt volutpat sit amet, hendrerit vitae quam. Nam varius in turpis vitae laoreet. Suspendisse mauris diam, pellentesque vitae quam vel, fringilla gravida nulla. Curabitur at commodo enim. Vivamus leo tellus, mattis vel aliquam non, porta in nibh. Aenean tortor ante, rutrum eu lacus ac, auctor condimentum est.</p>
+<p class="biggerText">Our expert in self development books. Feel free to ask him about any of those, 
+  he will surely have an opinion on your book!</p>
+<p>George is the president of our book club. He joined as soon as he started studying in 2018. 
+  He is one of the most passionate book person that you will ever meet. You can find loads of different 
+  things in his reading list, but you will surely see science fiction, self improvement and romance at 
+  the top of the list!</p>
 <img id="geo" class="profileimg" src="img/bookClubGeorge.jpg" alt="Portrait Photo"/>
 </div>
 
