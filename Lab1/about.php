5d1fe7f0564e19b1254e1158fc0a8ef4387a1f8e
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-01-24 16:09:14 +0200

    Making sure not all img is grayscale

--- Lab1/about.php
+++ Lab1/about.php
@@ -28,14 +28,14 @@
 <h1>Gintare Kunigonyte</h1>
 <p class="biggerText">One of our most valuable members in the book club. She has read more than 3 books. Her dedication is outstanding!</p>
 <p>Sed egestas fringilla tristique. Nullam neque nisi, pulvinar tincidunt volutpat sit amet, hendrerit vitae quam. Nam varius in turpis vitae laoreet. Suspendisse mauris diam, pellentesque vitae quam vel, fringilla gravida nulla. Curabitur at commodo enim. Vivamus leo tellus, mattis vel aliquam non, porta in nibh. Aenean tortor ante, rutrum eu lacus ac, auctor condimentum est.</p>
-<img id="gin" src="img/bookClubGintare.jpeg" alt="Portrait Photo"/>
+<img id="gin" class="profileimg" src="img/bookClubGintare.jpeg" alt="Portrait Photo"/>
 </div>
 
 <div class="right">
 <h1>Georgi Kolev</h1>
 <p class="biggerText">One of our most valuable members in the book club. She has read more than 3 books. Her dedication is outstanding!</p>
 <p>Sed egestas fringilla tristique. Nullam neque nisi, pulvinar tincidunt volutpat sit amet, hendrerit vitae quam. Nam varius in turpis vitae laoreet. Suspendisse mauris diam, pellentesque vitae quam vel, fringilla gravida nulla. Curabitur at commodo enim. Vivamus leo tellus, mattis vel aliquam non, porta in nibh. Aenean tortor ante, rutrum eu lacus ac, auctor condimentum est.</p>
-<img id="geo" src="img/bookClubGeorge.jpg" alt="Portrait Photo"/>
+<img id="geo" class="profileimg" src="img/bookClubGeorge.jpg" alt="Portrait Photo"/>
 </div>
 
 </div>
