ec2733148039ba3123d2719fed739e3aa94631d3
George Kolev <gogo1520180@gmail.com>
2021-01-26 12:25:45 +0200

    activeFix

--- Lab1/header.php
+++ Lab1/header.php
@@ -2,12 +2,23 @@
     <div id="siteheader">
         <img src="img/whiteLogo.png" alt="Logo"/>
 
+        <?php
+            $current_page = basename($_SERVER['PHP_SELF']);
+            // ASK WHY THIS DOESN'T WORK - A GUY ON THE INTERNET TOLD ME IT SHOULD
+            // $current_page = $_SERVER['REQUEST_URI'];
+
+            //PHP can't reach into the config file and read the basename($_SERVER['PHP_SELF])? 
+            //Globally defined variables ? 
+            //How do they work
+        ?>
+        
         <div class="mainnav">
-            <a class="<?php echo ($current_page == "index.php" || $current_page == "") ? "active" : " " ?>" href="index.php">Home</a>
-            <a class="<?php echo ($current_page == "about.php" || $current_page == "") ? "active" : " " ?>" href="about.php">About Us</a>
-            <a class="<?php echo ($current_page == "browse.php" || $current_page == "") ? "active" : " " ?>" href="browse.php">Browse</a>
-            <a class="<?php echo ($current_page == "mybooks.php" || $current_page == "") ? "active" : " " ?>" href="mybooks.php">My Books</a>
-            <a class="<?php echo ($current_page == "contact.php" || $current_page == "") ? "active" : " " ?>" href="contact.php">Contact</a>
+        
+            <a class="<?php echo ($current_page == "index.php" || $current_page == "") ? "active" : "" ?>" href="index.php">Home</a>
+            <a class="<?php echo ($current_page == "about.php" || $current_page == "") ? "active" : "" ?>" href="about.php">About Us</a>
+            <a class="<?php echo ($current_page == "browse.php" || $current_page == "") ? "active" : "" ?>" href="browse.php">Browse</a>
+            <a class="<?php echo ($current_page == "mybooks.php" || $current_page == "") ? "active" : "" ?>" href="mybooks.php">My Books</a>
+            <a class="<?php echo ($current_page == "contact.php" || $current_page == "") ? "active" : "" ?>" href="contact.php">Contact</a>
         </div>
     </div>
 </header>
