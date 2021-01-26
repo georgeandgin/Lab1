751a9851e8db69ea52a5e3829335f23e4e607f4b
Gintare Kunigonyte <gintare.kunigonyte@gmail.com>
2021-01-26 11:37:17 +0200

    browse

--- Lab1/browse.php
+++ Lab1/browse.php
@@ -22,13 +22,13 @@
 <div class="browse">
 
 <form action="">
-  <label for="fname">First name:</label><br>
+  <label for="fname">Author:</label><br>
   <input type="text" id="fname" name="fname" value=""><br>
-  <label for="lname">Last name:</label><br>
+  <label for="lname">Title:</label><br>
   <input type="text" id="lname" name="lname" value=""><br><br>
-  <input type="submit" value="Submit">
-</form
-> 
+  <input type="submit" value="Search">
+</form> 
+
 </div>
 </body>
 </html>
