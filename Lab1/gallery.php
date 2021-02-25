5ff4e8e6df775087f41c0ab7e9d96e733cf72319
George Kolev <gogo1520180@gmail.com>
2021-02-25 11:50:07 +0200

    grid/

--- Lab1/gallery.php
+++ Lab1/gallery.php
@@ -11,18 +11,121 @@
 
   <link rel="stylesheet" href="css/output.css">
 
+
+  <style>
+  #gallery { 
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5%;
+  }
+
+  .display {
+    padding: 10px;
+    width: 300px;
+    height: 300px;
+    vertical-align: middle;
+    filter: grayscale(100%);
+  }
+  </style>
+  <style>
+    * {
+  box-sizing: border-box;
+}
+
+/* Position the image container (needed to position the left and right arrows) */
+.container {
+  position: relative;
+}
+
+/* Hide the images by default */
+.mySlides {
+  display: none;
+}
+
+/* Add a pointer when hovering over the thumbnail images */
+.cursor {
+  cursor: pointer;
+}
+
+/* Next & previous buttons */
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* Container for image text */
+.caption-container {
+  text-align: center;
+  background-color: #222;
+  padding: 2px 16px;
+  color: white;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Six columns side by side */
+.column {
+  float: left;
+  width: 16.66%;
+}
+
+/* Add a transparency effect for thumnbail images */
+.demo {
+  opacity: 0.6;
+}
+
+.active,
+.demo:hover {
+  opacity: 1;
+}
+  </style>
+
+
 </head>
 
 <body>
 
 <?php include ('header.php');?>
 
-<header>
-    <img id="cover" src="img/bookClub.jpeg"/>
-    <img id="arrow" src="img/arrow.png"/>
-  </header>
-  <div class="main">
 
+  <div class="main">
+  <h2 style="margin:5%"> Your secret image uploads...</h2>
     <div id="gallery">
       <?php
 
@@ -33,19 +136,22 @@
         $stmt->execute();
 
         while ($stmt->fetch()) {
-          echo "<img src='gallery/" . $fileName . "'/>";
-          echo "$fileName";
+          echo "<img class='display' src='gallery/" . $fileName . "'/>";
         }
 
         $stmt->close();
 
        ?>
     </div>
+</div>
 
-
-  </div>
+<!-- Container for the image gallery -->
 
 </body>
+
+<script>
+
+</script>
 
 <?php include ('footer.php');?>
 
